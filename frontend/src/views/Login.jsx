c4b361e159c573685af868042b82a68cc331b8a7
mlynarz <mlynarz17@o2.pl>
2020-05-24 12:20:52 +0200

    register form updated

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -14,145 +14,148 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import {useHistory} from "react-router-dom";
+import {BlueLink} from "../components/StyledComponents";
 
 export function Login() {
-  const authContext = useContext(AuthenticationContext);
-  const history = useHistory();
-  const [auth, setAuth] = useState({
-    username: "",
-    password: "",
-  });
+    const authContext = useContext(AuthenticationContext);
+    const history = useHistory();
+    const [auth, setAuth] = useState({
+        username: "",
+        password: "",
+    });
 
-  function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-          {'Copyright © '}
-          <Link color="inherit" href="https://material-ui.com/">
-            SWI project
-          </Link>
-          {new Date().getFullYear()}
-        </Typography>
-    );
-  }
-
-  const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(1),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-  }));
-
-  const classes = useStyles();
-
-
-  return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline/>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon/>
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form
-              className={classes.form}
-              noValidate
-              onSubmit={(event) => {
-                fetch(`/api/login`, {
-                  method: "POST",
-                  body: JSON.stringify(auth),
-                  headers: [["Content-Type", "application/json"]],
-                })
-                    .then((res) => res.json())
-                    .then((res) => {
-                      setAuth({username: "", password: ""});
-                      authContext.setToken(res.token);
-                    })
-                    .then()
-                    .catch(() => {
-                      // setError
-                    });
-
-                event.preventDefault();
-              }}
-          >
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                autoComplete="username"
-                autoFocus
-                value={auth.username}
-                onChange={(e) => {
-                  const value = e.target.value;
-                  setAuth((s) => ({...s, username: value}));
-                }}
-            />
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                value={auth.password}
-                onChange={(e) => {
-                  const value = e.target.value;
-                  setAuth((s) => ({...s, password: value}));
-                  console.log(auth.password)
-                }}
-            />
-            <FormControlLabel
-                control={<Checkbox value="remember" color="primary"/>}
-                label="Remember me"
-            />
-            <Button
-                fullWidth
-                variant="contained"
-                color="primary"
-                type="submit"
-                className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                {/*<Link href="#" variant="body2">*/}
-                {/*  Forgot password?*/}
-                {/*</Link>*/}
-              </Grid>
-              <Grid item>
-                <Link onClick={() => history.push("/register")} variant="body2">
-                  {"Don't have an account? Sign Up"}
+    function Copyright() {
+        return (
+            <Typography variant="body2" color="textSecondary" align="center">
+                {'Copyright © '}
+                <Link color="inherit" href="https://material-ui.com/">
+                    SWI project
                 </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        <Box mt={8}>
-          <Copyright/>
-        </Box>
-      </Container>
-  );
+                {new Date().getFullYear()}
+            </Typography>
+        );
+    }
+
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+            marginTop: theme.spacing(8),
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        avatar: {
+            margin: theme.spacing(1),
+            backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+            width: '100%', // Fix IE 11 issue.
+            marginTop: theme.spacing(1),
+        },
+        submit: {
+            margin: theme.spacing(3, 0, 2),
+        },
+    }));
+
+    const classes = useStyles();
+
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form
+                    className={classes.form}
+                    noValidate
+                    onSubmit={(event) => {
+                        fetch(`/api/login`, {
+                            method: "POST",
+                            body: JSON.stringify(auth),
+                            headers: [["Content-Type", "application/json"]],
+                        })
+                            .then((res) => res.json())
+                            .then((res) => {
+                                setAuth({username: "", password: ""});
+                                authContext.setToken(res.token);
+                            })
+                            .then()
+                            .catch(() => {
+                                // setError
+                            });
+
+                        event.preventDefault();
+                    }}
+                >
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        name="username"
+                        autoComplete="username"
+                        autoFocus
+                        value={auth.username}
+                        onChange={(e) => {
+                            const value = e.target.value;
+                            setAuth((s) => ({...s, username: value}));
+                        }}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        value={auth.password}
+                        onChange={(e) => {
+                            const value = e.target.value;
+                            setAuth((s) => ({...s, password: value}));
+                        }}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary"/>}
+                        label="Remember me"
+                    />
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        className={classes.submit}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            {/*<Link href="#" variant="body2">*/}
+                            {/*  Forgot password?*/}
+                            {/*</Link>*/}
+                        </Grid>
+                        <Grid item>
+                            <Link
+                                onClick={() => history.push("/register")}
+                                variant="body2"
+                            >
+                                <a href={"/register"}>Don't have an account? Sign Up</a>
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            <Box mt={8}>
+                <Copyright/>
+            </Box>
+        </Container>
+    );
 }
