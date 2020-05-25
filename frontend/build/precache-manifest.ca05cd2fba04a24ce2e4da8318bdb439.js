bc948d5f16151a74f6bc962344060f4ba9a08bc4
Michal Karol <michal.p.karol@gmail.com>
2020-05-26 01:52:49 +0200

    Adding build

---
+++ frontend/build/precache-manifest.ca05cd2fba04a24ce2e4da8318bdb439.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467f7062a73e538ca2096cef9a92b1e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaf363022cb95ad91fd0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e6d6cfbb91bc9100b22b",
+    "url": "/static/css/main.0feaa43b.chunk.css"
+  },
+  {
+    "revision": "aaf363022cb95ad91fd0",
+    "url": "/static/js/2.56576a54.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.56576a54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d6cfbb91bc9100b22b",
+    "url": "/static/js/main.e148f6a6.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
